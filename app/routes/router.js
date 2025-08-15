a989625ec389e4830f19a3417b4002c5cc392f9d
Igor Andrade <rm94917@estudante.fieb.edu.br>
2025-08-15 08:29:19 -0300

    verifica se muda algua coisa msm

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,20 +1,23 @@
 var express = require("express");
 var router = express.Router();
+router.post("/html", function (req, res) {
+    res.json(req.body);
+})
 
+router.get("/banco-de-dados", function (req, res) {
+    res.render("pages/login")
+});
+router.get("/home", function (req, res) {
+    res.render("pages/home")
+});
 router.get("/", function (req, res) {
     res.render("pages/cadastro")
 });
 router.get("/sobre-api", function (req, res) {
     res.render("pages/doe")
 });
-router.get("/banco-de-dados", function (req, res) {
-    res.render("pages/login")
-});
 router.get("/autenticacao", function (req, res) {
     res.render("pages/serviços")
-});
-router.get("/home", function (req, res) {
-    res.render("pages/home")
 });
 router.get("/servidor", function (req, res) {
     res.render("pages/todos")
