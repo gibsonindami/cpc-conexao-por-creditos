098007fe7f7be1900cc73ae081beab588eeac413
Gibson <gibsonindami8@gmail.com>
2025-08-28 08:23:56 -0300

    Alterei

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,23 +1,29 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function (req, res) {
+router.get("/banco-de-dados", function (req, res) {
+    res.render("pages/login")
+});
+router.get("/banco-de-dados", function (req, res) {
     res.render("pages/cadastro")
 });
 router.get("/sobre-api", function (req, res) {
-    res.render("pages/doe")
-});
-router.get("/banco-de-dados", function (req, res) {
-    res.render("pages/login")
+    res.render("pages/home")
+
+
+
 });
 router.get("/autenticacao", function (req, res) {
     res.render("pages/serviços")
 });
-router.get("/home", function (req, res) {
-    res.render("pages/home")
-});
+
+
+
 router.get("/servidor", function (req, res) {
     res.render("pages/todos")
+});
+router.get("/servidor", function (req, res) {
+    res.render("pages/doe")
 });
 
 module.exports = router;
