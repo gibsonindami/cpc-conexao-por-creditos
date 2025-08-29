036029d88ab498ad3393155edfafd5551943a144
Gibson <gibsonindami8@gmail.com>
2025-08-29 09:40:27 -0300

    novas alterções

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,28 +1,23 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/banco-de-dados", function (req, res) {
+router.get("/login", function (req, res) {
     res.render("pages/login")
 });
-router.get("/banco-de-dados", function (req, res) {
+router.get("/cadastro", function (req, res) {
     res.render("pages/cadastro")
 });
-router.get("/sobre-api", function (req, res) {
+router.get("/home", function (req, res) {
     res.render("pages/home")
 
-
-
 });
-router.get("/autenticacao", function (req, res) {
+router.get("/serviços", function (req, res) {
     res.render("pages/serviços")
 });
-
-
-
-router.get("/servidor", function (req, res) {
+router.get("/todos", function (req, res) {
     res.render("pages/todos")
 });
-router.get("/servidor", function (req, res) {
+router.get("/doe", function (req, res) {
     res.render("pages/doe")
 });
 
