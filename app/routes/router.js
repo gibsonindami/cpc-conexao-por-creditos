77740913b456ec7420b091d8205fd60186f99517
Gibson <gibsonindami8@gmail.com>
2025-09-02 10:56:53 -0300

    resolvido

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,36 +1,28 @@
 var express = require("express");
 var router = express.Router();
-
+const { body, validationResult } = require("express-validator");
 router.post("/html", function (req, res) {
     res.json(req.body);
-});
+})
+ 
 router.get("/login", function (req, res) {
-    res.render("pages/login");
+    res.render("pages/login")
+});
+router.get("/", function (req, res) {
+    res.render("pages/home")
 });
 router.get("/cadastro", function (req, res) {
-    res.render("pages/cadastro");
+    res.render("pages/cadastro")
 });
-router.get("/home", function (req, res) {
-    res.render("pages/home");
+router.get("/doe", function (req, res) {
+    res.render("pages/doe")
 });
-router.get("/sobre-api", function (req, res) {
-    res.render("pages/doe");
-});
-router.get("/autenticacao", function (req, res) {
-    res.render("pages/serviços");
-});
-router.get("/servidor", function (req, res) {
-    res.send("Página do servidor");
+router.get("/servicos", function (req, res) {
+    res.render("pages/servicos")
 });
 router.get("/todos", function (req, res) {
-    res.render("pages/todos");
+    res.render("pages/todos")
 });
-
-router.get("/doe", function (req, res) {
-    res.render("pages/doe");
-});
-router.get("/banco-de-dados", function (req, res) {
-    res.render("pages/login");
-});
-
+ 
+ 
 module.exports = router;
