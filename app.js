b0f1a869c173388f027f5b106ef468b0930cf186
Gibson <gibsonindami8@gmail.com>
2025-09-02 10:34:43 -0300

    novo

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
-app.use(express.static("app/public"));
-
-app.set("view engine", "ejs");
-app.set("views", "./app/views");
-
-app.use(express.json()); 
+ 
 app.use(express.urlencoded({ extended: true }));
-
-var rotas = require("./app/routes/router");
-app.use("/", rotas);
-
-app.listen(port, () => {
-  console.log(`Servidor ouvindo na porta ${port}\nhttp://localhost:${port}`);
-});
+ 
+app.use(express.static("./app/public"));
+ 
+app.set("view engine", "ejs");
+app.set("views","./app/views");
+ 
+const rotaPrincipal = require("./app/routes/router");
+app.use("/", rotaPrincipal);
+ 
+ 
+app.listen(port, ()=>{
+    console.log(`Servidor online\nhttp://localhost:${port}`);
+})
--- app/routes/router.js
+++ app/routes/router.js
@@ -1,24 +1,36 @@
 var express = require("express");
 var router = express.Router();
 
+router.post("/html", function (req, res) {
+    res.json(req.body);
+});
 router.get("/login", function (req, res) {
-    res.render("pages/login")
+    res.render("pages/login");
 });
 router.get("/cadastro", function (req, res) {
-    res.render("pages/cadastro")
+    res.render("pages/cadastro");
 });
 router.get("/home", function (req, res) {
-    res.render("pages/home")
-
+    res.render("pages/home");
 });
-router.get("/serviços", function (req, res) {
-    res.render("pages/serviços")
+router.get("/sobre-api", function (req, res) {
+    res.render("pages/doe");
+});
+router.get("/autenticacao", function (req, res) {
+    res.render("pages/serviços");
+});
+router.get("/servidor", function (req, res) {
+    res.send("Página do servidor");
 });
 router.get("/todos", function (req, res) {
-    res.render("pages/todos")
+    res.render("pages/todos");
 });
+
 router.get("/doe", function (req, res) {
-    res.render("pages/doe")
+    res.render("pages/doe");
+});
+router.get("/banco-de-dados", function (req, res) {
+    res.render("pages/login");
 });
 
 module.exports = router;
