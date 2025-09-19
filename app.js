4c9c871ae645eb941afac4ca90790628037cad24
Andrade <rm94917@estudante.fieb.edu.br>
2025-09-19 10:44:21 +0000

    bj

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
+const port = process.env.PORT || 3000
 
 app.use(express.static("./app/public"));
 app.set("view engine", "ejs");
